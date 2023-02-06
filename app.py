8fc31e3d41d1065c3f5685b243d5284188043f9a
tMology <89882702+tMology@users.noreply.github.com>
2023-02-06 10:49:13 -0500

    About Page Updated

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World this my first program i was write the first python prohramming langauge"
+    return "Hello World this my first program i was write the first python programming langauge"
 if __name__ == "__main__":
     app.run(host ='127.0.0.1', port =8098) 
